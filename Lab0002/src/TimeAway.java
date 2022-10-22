506aa821ae476e43588f241f8a0d30753a50fe2c
Steven Henry <stevenhenry012@gmail.com>
2022-10-22 14:24:49 -0400

    Added code for lab 2 that allows to view saved dates Added extra functionality to Lab 2 Code that allows users to choose between saved dates and future dates Added comments for lab 2 code

--- Lab0002/src/TimeAway.java
+++ Lab0002/src/TimeAway.java
@@ -1,10 +1,10 @@
+// Imports API's to fulfill program function
 import java.time.LocalDate;
-import java.time.Period;
 import java.util.Scanner;
-
 import static java.time.temporal.ChronoUnit.DAYS;
 
 public class TimeAway {
+    //Constant Saved Data
     public static LocalDate Lab1 = LocalDate.of(2022, 9, 28);
     public static LocalDate Lab2 = LocalDate.of(2022, 10, 28);
     public static LocalDate Lab3 = LocalDate.of(2022, 11, 23);
@@ -14,11 +14,15 @@ public class TimeAway {
     public static LocalDate quiz3 = LocalDate.of(2022,10,26);
     public static LocalDate quiz4 = LocalDate.of(2022,11,7);
     public static LocalDate quiz5 = LocalDate.of(2022,12,7);
-    public static LocalDate userDate = LocalDate.of(0001,01,01);
-    public static LocalDate mockDate;
     public static LocalDate[] dateList = {Lab1, Lab2, Lab3, Lab4, quiz1, quiz2, quiz3, quiz4, quiz5};
+    // Mutable Data
+    public static LocalDate userDate = LocalDate.of(1,1,1);
+    public static LocalDate mockDate;
 
 
+    //Splits the user's entries by the "/"'s they used when they entered it and added it to the array.
+    // Constructs a LocalDate from the parts of the array that respectively represent the month, day and year
+    // Prints out the user date to let them know what it has been set too.
     public static void setUserDate(String uD)
     {
         String[] partOfDate = uD.split("/");
@@ -28,6 +32,9 @@ public class TimeAway {
         userDate = LocalDate.of(year, month, day);
         System.out.println("Your date has been set as " +userDate);
     }
+    //Splits the user's entries by the "/"'s they used when they entered it and added it to the array.
+    // Constructs a LocalDate from the parts of the array that respectively represent the month, day and year
+    // Prints out the mock date to let them know what it has been set too.
     public static void setMockDate(String uD)
     {
         String[] partOfDate = uD.split("/");
@@ -37,14 +44,14 @@ public class TimeAway {
         mockDate = LocalDate.of(year, month, day);
         System.out.println("Your mock date has been set as " + mockDate);
     }
-
+    // Calculates the day between dates
     public static long daysBetweenDates(LocalDate a, LocalDate b)
     {
         return a.until(b, DAYS);
     }
 
-
-    public static void printDates()
+    //prints out the list of Saved Dates
+    public static void printSavedDates()
     {
         for ( int i = 1; i < dateList.length + 1; i++)
         {
@@ -58,15 +65,17 @@ public class TimeAway {
             }
         }
     }
+    //prints out the menu options
     public static void printMenu()
     {
         System.out.println("Hello user what would you like to do?");
         System.out.println("1.View Saved Dates" );
         System.out.println("2.Create your own Date? Please enter in the format of MM/DD/YYYY include slashes." );
         System.out.println("3.Compare your date to Saved Date?" );
-        System.out.println("3.Compare your date to a random date?" );
+        System.out.println("4.Compare your date to another date?" );
+        System.out.println("5.End program" );
     }
-
+// main function that constantly reads user input.
     public static void main(String[] args) {
 
         printMenu();
@@ -79,10 +88,11 @@ public class TimeAway {
             switch(choice)
             {
                 case 1:
-                    printDates();
+                    printSavedDates();
                     printMenu();
                     break;
                 case 2:
+                    System.out.println("Enter your date:");
                     String date = input.next();
                     setUserDate(date);
                     printMenu();
@@ -91,7 +101,7 @@ public class TimeAway {
                     System.out.println("Input the number of the from Saved Dates you want to compare.");
                     int sD = input.nextInt();
                     if (userDate != null) {
-                        System.out.println("The amount of days between "+ userDate + " " + dateList[sD] + " is " + daysBetweenDates(userDate , dateList[sD]));
+                        System.out.println("The amount of days between "+ userDate + " " + dateList[sD-1] + " is " + daysBetweenDates(userDate , dateList[sD-1]));
                         printMenu();
                         break;
                     }
@@ -102,6 +112,17 @@ public class TimeAway {
                     System.out.println("The amount of days between your first date "+ userDate + " and your " + mockDate + " is " + daysBetweenDates(userDate ,mockDate));
                     printMenu();
                     break;
+                case 5:
+                    System.out.println("Thank you");
+                    try {
+                        input.close();
+                    }
+                        catch(IllegalStateException e)
+                        {
+                           System.out.println("Program Closed");
+                        }
+                    break;
+
 
 
 
